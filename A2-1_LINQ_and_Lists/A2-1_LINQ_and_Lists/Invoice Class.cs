864b60508d45cd4b4338f1d6f218e86062427892
rhaney93 <ryanhaney0493@gmail.com>
2022-06-13 21:27:10 -0600

    WTF is wrong with line 76

---
+++ A2-1_LINQ_and_Lists/A2-1_LINQ_and_Lists/Invoice Class.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A2_1_LINQ_and_Lists
+{
+    // Exercise 9.3 Solution: Invoice.cs
+    // Invoice class.
+    public class Invoice
+    {
+        // declare variables for Invoice object
+        private int quantityValue;
+        private decimal priceValue;
+
+        // auto-implemented property PartNumber
+        public int PartNumber { get; set; }
+
+        // auto-implemented property PartDescription
+        public string PartDescription { get; set; }
+
+        // four-argument constructor
+        public Invoice(int part, string description,
+           int count, decimal pricePerItem)
+        {
+            PartNumber = part;
+            PartDescription = description;
+            Quantity = count;
+            Price = pricePerItem;
+        }
+
+        // property for quantityValue; ensures value is positive
+        public int Quantity
+        {
+            get
+            {
+                return quantityValue;
+            }
+            set
+            {
+                if (value > 0) // determine whether quantity is positive
+                {
+                    quantityValue = value; // valid quantity assigned
+                }
+            }
+        }
+
+        // property for pricePerItemValue; ensures value is positive
+        public decimal Price
+        {
+            get
+            {
+                return priceValue;
+            }
+            set
+            {
+                if (value >= 0M) // determine whether price is non-negative
+                {
+                    priceValue = value; // valid price assigned
+                }
+            }
+        }
+
+        // return string containing the fields in the Invoice in a nice format
+        public override string ToString()
+        {
+            // left justify each field, and give large enough spaces so
+            // all the columns line up
+            return $"{PartNumber,-5} {PartDescription,-20} {Quantity,-5} {Price,6:C}";
+        }
+        
+    }
+
+    public class List
+    {
+        public static List<Invoice> invoice = new List<Invoice>()
+        {
+        invoice.Add(new Invoice {PartNumber = 83, PartDescription = "Electric Sander", Quantity = 7, Price = 57.98M})
+
+        }
+    }
+}
---
+++ A2-1_LINQ_and_Lists/LINQ_List/List.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQ_List
+{
+    public class List
+    {
+        static void Main(string[] args);
+
+
+        public static List<Invoice> invoice = new List<Invoice>()
+        {
+        invoice.Add(new Invoice {PartNumber = 83, PartDescription = "Electric Sander", Quantity = 7, Price = 57.98M})
+
+        }
+    }
+}
